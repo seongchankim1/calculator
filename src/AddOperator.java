bc49245438982c7fe84a35d656d25fc967b964b7
seongchankim1 <167050690+seongchankim1@users.noreply.github.com>
2024-05-03 12:39:51 +0900

    계산기 피드백 반영
    인터페이스 구현

--- src/AddOperator.java
+++
@@ -1,7 +0,0 @@
-public class AddOperator {
-
-    public double operate(int number1, int number2) {
-        double result = number1 + number2;
-        return result;
-    }
-}
--- src/MultiplyOperator.java
+++
@@ -1,9 +0,0 @@
-public class MultiplyOperator {
-
-    public double operate(int number1, int number2) {
-        double result = number1 * number2;
-        return result;
-    }
-}
-
-
